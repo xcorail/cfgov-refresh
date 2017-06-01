32726beacb817e1dade26af672087cdf4435280f
Andy Chosak <chosak@users.noreply.github.com>
2017-05-31 20:38:05 -0400

    squash the first 40 migrations

--- cfgov/v1/migrations/0040_fill_filter_spec.py
+++ cfgov/v1/migrations/0040_fill_filter_spec.py
@@ -5,13 +5,24 @@ from django.db import migrations, models
 from wagtail.wagtailimages.utils import get_fill_filter_spec_migrations
 
 
+# See https://github.com/wagtail/wagtail/issues/3259 for why this is necessary.
+wforward, wreverse = get_fill_filter_spec_migrations('v1', 'CFGOVRendition')
+
+
+def forward(apps, schema_migration):
+    return wforward(apps, schema_migration)
+
+
+def reverse(apps, schema_migration):
+    return wreverse(apps, schema_migration)
+
+
 class Migration(migrations.Migration):
 
 	dependencies = [
 		('v1', '0039_add_filter_spec_to_cfgovrendition'),
 	]
 
-	forward, reverse = get_fill_filter_spec_migrations('v1', 'CFGOVRendition')
 	operations = [
 		migrations.RunPython(forward, reverse),
 	]
